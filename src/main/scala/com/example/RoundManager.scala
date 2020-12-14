1f7cd9a4e10a065f0831639ff79470b6036d98fb
Hock Shem <hockshem.c@gmail.com>
2020-12-14 23:51:57 +0800

    Fixed RoundManager compilation error and commented out HelloClient and HelloServer
    Can start writing test cases for this source file

--- src/main/scala/com/example/RoundManager.scala
+++ src/main/scala/com/example/RoundManager.scala
@@ -13,7 +13,6 @@ import scala.util.{Failure, Success}
 
 
 object RoundManager {
-    sealed trait GameCommands 
     // Logic specific to Rock-Paper-Scissors
     sealed trait RockPaperScissorsCommands {
         def winAgainst(opponentSelection: RockPaperScissorsCommands): RockPaperScissorsResults
@@ -23,6 +22,17 @@ object RoundManager {
     final case object Lose extends RockPaperScissorsResults
     final case object Victory extends RockPaperScissorsResults
     final case object Tie extends RockPaperScissorsResults
+
+    final case object Rock extends RockPaperScissorsCommands {
+        def winAgainst(opponentSelection: RockPaperScissorsCommands): RockPaperScissorsResults = {
+            opponentSelection match {
+                case Rock => Tie
+                case Paper => Lose
+                case Scissors => Victory
+                case NotSelected => Victory
+            }
+        }
+    }
     
     final case object Paper extends RockPaperScissorsCommands {
         def winAgainst(opponentSelection: RockPaperScissorsCommands): RockPaperScissorsResults = {
@@ -63,35 +73,37 @@ object RoundManager {
         selectionMap.head._2.winAgainst(selectionMap.last._2)
     }
 
-    // Messages that the actor can process
-    final case class RockPaperScissorsSelection(fromPlayer: ActorRef[GameSessionManager.GameSessionResponses], selection: RockPaperScissorsCommands) extends GameCommands
-    final case object AllPlayersSelected extends GameCommands
-
+    sealed trait RoundManagerCommands
     sealed trait RoundManagerResponses
-    final case class RockPaperScissorsSelectionRequest(roundManager: ActorRef[GameCommands]) extends RoundManagerResponses
-    final case class GameStatusUpdate(roundWinner: ActorRef[GameSessionManager.GameSessionResponses], roundLoser: ActorRef[GameSessionManager.GameSessionResponses]) extends RoundManagerResponses
+    
+    final case class RockPaperScissorsSelection(fromPlayer: ActorRef[RoundManagerResponses], selection: RockPaperScissorsCommands) extends RoundManagerCommands
+    final case class RockPaperScissorsSelectionRequest(roundManager: ActorRef[RoundManagerCommands]) extends RoundManagerResponses
+
+    final case object AllPlayersSelected extends RoundManagerCommands
+
+    final case class GameStatusUpdate(roundWinner: ActorRef[RoundManagerResponses], roundLoser: ActorRef[RoundManagerResponses]) extends RoundManagerResponses
     final case object GameStatusUnchanged extends RoundManagerResponses
     
-    def apply(manager: ActorRef[GameSessionManager.GameSessionRequests], players: Seq[ActorRef[RoundManagerResponses]]): Behavior[GameCommands] = {
-        Behaviors.setup { context => new RoundManager(context, manager)}
+    def apply(gameSessionManager: ActorRef[RoundManagerResponses], players: Seq[ActorRef[RoundManagerResponses]]): Behavior[RoundManagerCommands] = {
+        Behaviors.setup { context => new RoundManager(context, gameSessionManager, players)}
     }
 }
 
-class RoundManager(context: ActorContext[RoundManager.GameCommands], gameSessionManager: ActorRef[GameSessionManager.GameSessionRequests]) extends AbstractBehavior(context) {
+class RoundManager(context: ActorContext[RoundManager.RoundManagerCommands], gameSessionManager: ActorRef[RoundManager.RoundManagerResponses], players: Seq[ActorRef[RoundManager.RoundManagerResponses]]) extends AbstractBehavior(context) {
     import RoundManager._
-    var thisPlayer: Option[ActorRef[GameSessionManager.GameSessionResponses]] = None
-    var thatPlayer: Option[ActorRef[GameSessionManager.GameSessionResponses]] = None
-    override def onMessage(msg: GameCommands): Behavior[GameCommands] = {
+    
+    val thisPlayer = players.head
+    val thatPlayer = players.last
+    playerSelectionMap += (thisPlayer.path.toString() -> NotSelected)
+    playerSelectionMap += (thatPlayer.path.toString() -> NotSelected)
+
+    override def onMessage(msg: RoundManagerCommands): Behavior[RoundManagerCommands] = {
         msg match {
-            case PlayersPair(p, o) =>
-                thisPlayer = Some(p)
-                thatPlayer = Some(o)
-                p ! RockPaperScissorsSelectionRequest(context.self)
-                o ! RockPaperScissorsSelectionRequest(context.self)
-                this
-            case RockPaperScissorsSelection(player, selection) =>
+            // When one of the player has made a selection
+            case RockPaperScissorsSelection(player, selection) =>            
                 playerSelectionMap += (player.path.toString() -> selection)
-                if (playerSelectionMap.contains(thisPlayer.get.path.toString()) && playerSelectionMap.contains(thatPlayer.get.path.toString())) {
+                // If both of the players have already responded
+                if (!playerSelectionMap.valuesIterator.contains(NotSelected)) {
                     context.self ! AllPlayersSelected
                 }
                 this
@@ -99,13 +111,13 @@ class RoundManager(context: ActorContext[RoundManager.GameCommands], gameSession
                 val result = winnerSelectionRule(playerSelectionMap)
                 result match {
                     case Lose => 
-                        gameSessionManager ! GameStatusUpdate(thatPlayer.get, thisPlayer.get)
+                        gameSessionManager ! GameStatusUpdate(thatPlayer, thisPlayer)
                     case Victory => 
-                        gameSessionManager ! GameStatusUpdate(thisPlayer.get, thatPlayer.get)
+                        gameSessionManager ! GameStatusUpdate(thisPlayer, thatPlayer)
                     case Tie => 
                         gameSessionManager ! GameStatusUnchanged
                 }
-                
+                this
         }
     }
 }
